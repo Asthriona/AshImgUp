e2f4ca0adecaff2e07cfa2712393c9f3195a11b5
Asthriona <im@asthriona.com>
2020-07-13 13:16:18 +0200

    better ID

--- routes/imgs.js
+++ routes/imgs.js
@@ -2,10 +2,12 @@ const imageUpload = require('../models/imageUpload');
 const router = module.exports = require('express').Router();
 const path = require('path')
 router.get('/:imagefile', (req,res)=>{
-    let {imagefile} = req.params
-    let id = imagefile.split(".").shift();
-    imageUpload.findById(id)
-    .then(v =>{
-        res.render('img', {image: v.filePath})
-    }).catch(err => console.log(err).then(res.status(404).send("oof! an error happened :/")))
+    let imagefile = req.params
+    let id = imagefile;
+    imageUpload.findOne({
+        filePath: req.params
+    }, function(v){
+        console.log()
+        res.render('img', {image: "/upload/"+req.params.imagefile})
+    });
 })
--- routes/upload.js
+++ routes/upload.js
@@ -1,16 +1,18 @@
 const router = module.exports = require('express').Router();
 const multer = require('multer');
 const imageUpload = require('../models/imageUpload');
+var { nanoid } = require("nanoid");
 const storage = multer.diskStorage({
     destination: (req,file,cb)=>{
         cb(null, './public/upload/')
     },
     filename: (req,file,cb)=>{
+        let id = nanoid(6);
         let img = new imageUpload({});
-        img.filePath = "/upload/" + img._id+"."+file.originalname.split('.').pop();
+        img.filePath = "/upload/" + id+"."+file.originalname.split('.').pop();
         img.save().then(()=>{
             console.log("saved!");
-            cb(null, img._id+"."+file.originalname.split('.').pop());
+            cb(null, id+"."+file.originalname.split('.').pop());
         })
         .catch(err => console.log(err));
         

5874b9e78c1cf053eded07db8f09aea5bb5bf1ce
Asthriona <im@asthriona.com>
2020-07-15 14:39:34 +0200

    new production fix 2.0! D:

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,7 @@ uploadbtn.addEventListener('click', ()=>{
     }else{
         let formData = new FormData();
         formData.append('photo', input.files[0])
-        fetch('http://rev.asthriona.com:3000/upload',{
+        fetch('/upload',{
             method: 'POST',
             body: formData
         }).then(res => res.json())
@@ -17,23 +17,10 @@ uploadbtn.addEventListener('click', ()=>{
             let div = document.querySelector('div.img');
             let link = document.createElement('a');
             let linkText = document.createTextNode('Here is your link! :)')
-            link.href = 'http://aiu.asthriona.com/upload'+json.fileName;
+            link.href = '/upload/'+json.fileName;
             link.appendChild(linkText)
             div.appendChild(link)
         })
         .catch(err => console.log(err));
     }
-})
-
-
-//Footer code
-var oldTime = new Date('2019/11/11 19:04:00');
-var timer = setInterval(function () {
-  var nowTime = new Date();
-  var longTime = nowTime - oldTime;
-  var days = parseInt(longTime / 1000 / 60 / 60 / 24, 10);
-  var hours = parseInt(longTime / 1000 / 60 / 60 % 24, 10);
-  var minutes = parseInt(longTime / 1000 / 60 % 60, 10);
-  var seconds = parseInt(longTime / 1000 % 60, 10);
-  $('.uptime').html(longTime = days + " Days " + hours + " Hours " + minutes + " Minutes and " + seconds + " seconds");
-}, 1000);
+});
